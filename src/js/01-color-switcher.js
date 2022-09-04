901067ff833eec7aa96db808d07de9d8100bf267
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-09-04 17:18:49 +0300

    goit-js-hw-09
    Виправлено баг швидкого натискання декілька разів на кнопку 'start'

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,18 +6,27 @@ const changeColorStart = document.querySelector('[data-start]');
 const changeColorStop = document.querySelector('[data-stop]');
 // const changeColorBody = document.querySelector('body');
 
+changeColorStop.disabled = true;
+let timerId = null;
+
 changeColorStart.addEventListener('click', (e) => {
+    changeColorStart.disabled = true;
+    changeColorStop.disabled = false;
     const timerId = setInterval(function() {
         document.body.style.backgroundColor = getRandomHexColor();
-        changeColorStart.setAttribute('disabled', false);
-        if (setInterval) {
-            changeColorStart.setAttribute('disabled', false);
-        }
+        // changeColorStart.setAttribute('disabled', false);
+        
+        // if (setInterval) {
+            
+        //     changeColorStart.setAttribute('disabled', false);
+        // }
     }, 1000);
     
     changeColorStop.addEventListener('click', (e) => {
         clearInterval(timerId);
-        changeColorStart.removeAttribute('disabled');
+        changeColorStart.disabled = false;
+        changeColorStop.disabled = true;
+        // changeColorStart.removeAttribute('disabled');
     });
 });
 
